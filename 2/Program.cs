1a170d03118f1d75845080cd1412dd180af7aa55
Ilya <ilya-osipchuk@inbox.ru>
2023-03-15 19:50:20 +0300

    исправил  вторую  задачу

--- 2/Program.cs
+++ 2/Program.cs
@@ -17,7 +17,7 @@ int [] GetArray(int size)
     
     for(int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(100);
+        result[i] = new Random().Next(10);
     }
 return result;
 }
@@ -26,9 +26,9 @@ int sum = 0;
 
 for(int i = 0; i < size; i++)
 {
-    if(i %2 == 0)
+    if(i %2 != 0)
 
     sum += array[i];
 }
 
-Console.WriteLine($"Массив [{String.Join("; ", array)}] сумма чисел  четным имндексом равна {sum}");
+Console.WriteLine($"Массив [{String.Join("; ", array)}] сумма чисел с  четным индексом равна {sum}");
